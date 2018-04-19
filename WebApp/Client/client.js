d506dd09667f4e1fb79deaabf513c729cc96fae5
Joppy Furr <joppyfurr@gmail.com>
2018-04-20 00:05:57 +1200

    Move the client browser-side javascript into its own file

---
+++ WebApp/Client/client.js
@@ -0,0 +1,11 @@
+/* PokéSAG browser implementation */
+class PokeSAG_Client extends React.Component {
+
+    constructor () {
+        super ();
+    }
+
+    render () {
+        return <h1> Hello from PokeSAG_Client </h1>
+    }
+}
