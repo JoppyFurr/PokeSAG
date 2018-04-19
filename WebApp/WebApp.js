3025a86aabca2456b07807d2cf632f78b319e458
Joppy Furr <joppyfurr@gmail.com>
2018-04-19 22:56:32 +1200

    Database connection for the WebApp

--- WebApp/WebApp.js
+++ WebApp/WebApp.js
@@ -1,14 +1,23 @@
 /*
  * Joppy Furr, 2018
  */
+const express = require ('express');
+const sqlite3 = require ('sqlite3').verbose ();
+
+/* Database connection */
+let db = new sqlite3.Database('../pages.sqlite3', (error) => {
+    if (error) {
+        return console.error (error.message);
+    }
+    console.log ('PokéSAG WebApp connected to database file pages.sqlite3');
+});
 
 /* HTTP Server */
-var express = require ("express"),
-    app = express (),
-    port = process.env.PORT || 8080;
+let app = express ();
+let port = process.env.PORT || 8080;
 
-app.use (express.static ("Static", { "index": ["main.html"] } ));
+app.use (express.static ('Static', { 'index': ['main.html'] } ));
 
-var server = app.listen (port, "::", function () {
-    console.log ("Listening on port %s.", server.address ().port);
+let server = app.listen (port, '::', function () {
+    console.log ('Listening on port %s.', server.address ().port);
 });
