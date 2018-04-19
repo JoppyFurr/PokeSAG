b70069cb608d18e7f326596383e72ad7ba6242cd
Joppy Furr <joppyfurr@gmail.com>
2018-04-19 23:32:52 +1200

    Initial API to retrieve pages from the server

--- WebApp/WebApp.js
+++ WebApp/WebApp.js
@@ -4,19 +4,37 @@
 const express = require ('express');
 const sqlite3 = require ('sqlite3').verbose ();
 
-/* Database connection */
-let db = new sqlite3.Database('../pages.sqlite3', (error) => {
+/***********************
+ * Database Connection *
+ ***********************/
+
+let db = new sqlite3.Database('../pages.sqlite3', sqlite3.OPEN_READONLY, (error) => {
     if (error) {
         return console.error (error.message);
     }
     console.log ('PokéSAG WebApp connected to database file pages.sqlite3');
 });
 
-/* HTTP Server */
+
+/***************
+ * HTTP Server *
+ ***************/
+
 let app = express ();
 let port = process.env.PORT || 8080;
 
-app.use (express.static ('Static', { 'index': ['main.html'] } ));
+app.use (express.static ('Client', { 'index': ['main.html'] } ));
+
+/* API to retrieve pages */
+/* TODO: Come up with something faster, like sending 100 pages at a time */
+app.get ('/Pages/', function onListenEvent (req, res) {
+    db.all ('select * from pages order by rx_date', [], (error, rows) => {
+        if (error) {
+            throw error;
+        }
+        res.send (rows);
+    });
+});
 
 let server = app.listen (port, '::', function () {
     console.log ('Listening on port %s.', server.address ().port);
