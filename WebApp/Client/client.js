2c2ec7b7703481b11d37cc4139755c8e32741b4f
Joppy Furr <joppyfurr@gmail.com>
2018-04-20 22:01:46 +1200

    Getting the pages onto the screen

--- WebApp/Client/client.js
+++ WebApp/Client/client.js
@@ -1,11 +1,39 @@
 /* PokéSAG browser implementation */
-class PokeSAG_Client extends React.Component {
+class PokeSAG_Client extends React.Component
+{
 
-    constructor () {
+    constructor ()
+    {
         super ();
+
+        this.state = {
+            pages_database: []
+        };
+
+        this.refresh_data = this.refresh_data.bind (this);
     }
 
-    render () {
-        return <h1> Hello from PokeSAG_Client </h1>
+    refresh_data (e)
+    {
+        fetch ('/Pages/')
+            .then ( result => {
+                result.json()
+                    .then ( json => {
+                        this.setState ( { pages_database: json } );
+                    });
+            });
+    }
+
+    componentDidMount ()
+    {
+        this.refresh_data (null);
+    }
+
+    render ()
+    {
+        let pages = this.state.pages_database.map ( page => {
+            return <p>{page.content}</p>
+        });
+        return <div className="page_list">{pages}</div>
     }
 }
