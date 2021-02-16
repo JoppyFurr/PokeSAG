9e5806408a996e28010c2e8eaf82cff0c7e9d25b
Luke Rogers <luke@dmptr.com>
2021-02-16 16:07:19 +1300

    Add (optional) full-text searching.

--- WebApp/WebApp-postgres.js
+++ WebApp/WebApp-postgres.js
@@ -54,14 +54,27 @@ app.get ('/Pages/', function onListenEvent (req, res) {
 });
 
 /* API to retrieve all pages matching a string */
-app.get ('/Pages/Search/:string/', function onListenEvent (req, res) {
-    db.query ("select * from pages where content ilike $1 order by rx_date desc", ['%' + req.params.string + '%'], (query_err, query_res) => {
-        if (query_err) {
-            throw query_err;
-        }
-        clean_rows (query_res.rows);
-        res.send (query_res.rows);
-    });
+app.get ('/Pages/Search/:type/:string/', function onListenEvent (req, res) {
+    if (req.params.type == 'ft') {
+        let search_string = decodeURIComponent(req.params.string);
+        db.query ("SELECT * from pages WHERE tsx @@ websearch_to_tsquery('simple', $1) ORDER BY rx_date DESC LIMIT 100", [search_string], (query_err, query_res) => {
+            if (query_err) {
+                throw query_err;
+            }
+            clean_rows (query_res.rows);
+            res.send (query_res.rows);
+        });
+    } else {
+        let search_string = decodeURIComponent(req.params.string).replace (/[#%.?\/\\]/g, '');
+        db.query ("select * from pages where content ilike $1 order by rx_date desc",['%' + search_string + '%'], (query_err, query_res) => {
+            if (query_err) {
+                throw query_err;
+            }
+            clean_rows (query_res.rows);
+            res.send (query_res.rows);
+        });
+    }
+
 });
 
 let server = app.listen (port, '::', function () {
--- pokesag-postgres.lua
+++ pokesag-postgres.lua
@@ -24,34 +24,62 @@ function create_database ()
     local db = postgres.connectdb (
         'postgresql://' .. username .. ':' .. password .. '@' .. path)
 
-    if db:status() == postgres.CONNECTION_OK then
-        print ('Connected to database.')
-    else
-        print ('Error connecting to database.')
-        print (db:errorMessage())
-        return false
-    end
-
-    local rc = db:exec [[
-        create table pages (
-            rx_date     timestamp   not null,
-            source      text        not null,
-            recipient   text        not null,
-            content     text        not null);
-    ]]
-
-    if rc:status() == postgres.PGRES_COMMAND_OK then
-        print ('Table created.')
-    elseif string.match(rc:errorMessage (), 'already exists') then
-        print ('Table detected.')
-    else
-        print ('Error creating table.')
-        print (rc:errorMessage ())
-        return false
-    end
-
-    db:finish ()
-    return true
+        if db:status() == postgres.CONNECTION_OK then
+            print ('Connected to database.')
+        else
+            print ('Error connecting to database.')
+            print (db:errorMessage())
+            return false
+        end
+    
+    
+        print ('Attempting to create table...')
+        local rc = db:exec [[
+            CREATE TABLE IF NOT EXISTS pages (
+                rx_date     timestamp   not null,
+                source      text        not null,
+                recipient   text        not null,
+                content     text        not null);
+        ]]
+    
+        if rc:status() == postgres.PGRES_COMMAND_OK then
+            print ('...table okay.')
+        else
+            print ('Error creating table.')
+            print (rc:errorMessage ())
+            return false
+        end
+    
+        print ('Attempting to create search index column...')
+        local rc = db:exec [[
+            ALTER TABLE pages
+            ADD COLUMN IF NOT EXISTS tsx tsvector
+            GENERATED ALWAYS AS (to_tsvector('simple', content)) STORED;
+        ]]
+    
+        if rc:status() == postgres.PGRES_COMMAND_OK then
+            print ('...search index column okay.')
+        else
+            print ('Error creating search index column.')
+            print (rc:errorMessage ())
+            return false
+        end
+    
+        print ('Attempting to create search index...')
+        local rc = db:exec [[
+            CREATE INDEX IF NOT EXISTS search_idx ON pages USING GIN (tsx);
+        ]]
+    
+        if rc:status() == postgres.PGRES_COMMAND_OK then
+            print ('...search index okay.')
+        else
+            print ('Error creating search index.')
+            print (rc:errorMessage ())
+            return false
+        end
+    
+        db:finish ()
+        return true
 end
 
 -- Use a prepared statement to store a page in the database
