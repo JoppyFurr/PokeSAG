a01a22595e45045643d7d697045e47dcb5ea0391
Luke Rogers <luke@dmptr.com>
2021-02-23 10:46:45 +1300

    Remove binds.

--- WebApp/Client/client.js
+++ WebApp/Client/client.js
@@ -19,17 +19,9 @@ class PokeSAG_Client extends React.Component
             search_type: "ft",
             search_type_class: "setting green",
         };
-
-        this.refresh_data = this.refresh_data.bind (this);
-        this.update_search_string = this.update_search_string.bind (this);
-        this.handle_search = this.handle_search.bind (this);
-        this.toggle_settings = this.toggle_settings.bind (this);
-        this.toggle_auto_refresh = this.toggle_auto_refresh.bind (this);
-        this.toggle_search_type = this.toggle_search_type.bind (this);
     }
 
-
-    update_search_string (e)
+    update_search_string = () =>
     {
         this.setState ( { search_string: e.target.value } );
 
@@ -40,7 +32,7 @@ class PokeSAG_Client extends React.Component
         }
     }
 
-    handle_search (e)
+    handle_search = () =>
     {
         if (e.key === 'Enter' && this.state.search_string != '')
         {
@@ -49,7 +41,7 @@ class PokeSAG_Client extends React.Component
         }
     }
 
-    refresh_data (e)
+    refresh_data = () =>
     {
         switch (this.state.mode)
         {
@@ -75,13 +67,8 @@ class PokeSAG_Client extends React.Component
         }
     }
 
-    componentDidMount ()
-    {
-        this.refresh_data (null);
-    }
-
     /* Toggle whether the settings are visible or not */
-    toggle_settings ()
+    toggle_settings = () =>
     {
         if (this.state.settings_class == "settings hidden")
         {
@@ -95,7 +82,7 @@ class PokeSAG_Client extends React.Component
         }
     }
 
-    toggle_search_type ()
+    toggle_search_type = () =>
     {
         if (this.state.search_type == 'ft')
         {
@@ -111,7 +98,7 @@ class PokeSAG_Client extends React.Component
         }
     }
 
-    toggle_auto_refresh ()
+    toggle_auto_refresh = () =>
     {
         if (this.state.auto_refresh == false)
         {
@@ -126,6 +113,11 @@ class PokeSAG_Client extends React.Component
             this.state.auto_refresh_timer = null;
             this.setState({auto_refresh_class: "setting red"});
         }
+    }
+
+    componentDidMount ()
+    {
+        this.refresh_data (null);
     }
 
     render ()
