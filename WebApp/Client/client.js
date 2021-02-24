936863e52d9260092b957ab169fd136cb895f73d
Joppy Furr <joppyfurr@gmail.com>
2021-02-24 12:04:57 +0000

    Formatting tidyup for WebApp client

--- WebApp/Client/client.js
+++ WebApp/Client/client.js
@@ -22,7 +22,7 @@ class PokeSAG_Client extends React.Component
 
     update_search_string = (e) =>
     {
-        this.setState ( { search_string: e.target.value } );
+        this.setState ({ search_string: e.target.value });
 
         if (e.target.value == '')
         {
@@ -46,11 +46,11 @@ class PokeSAG_Client extends React.Component
         {
             case 'search':
                 let search_type = this.state.full_text_search ? 'ft' : 'basic';
-                fetch ('/Pages/Search/' + search_type + '/' + encodeURIComponent(this.state.search_string) + '/')
+                fetch ('/Pages/Search/' + search_type + '/' + encodeURIComponent (this.state.search_string) + '/')
                     .then ( result => {
-                        result.json()
+                        result.json ()
                             .then ( json => {
-                                this.setState ( { pages_database: json } );
+                                this.setState ({ pages_database: json });
                             });
                     });
                 break;
@@ -59,9 +59,9 @@ class PokeSAG_Client extends React.Component
             default:
                 fetch ('/Pages/')
                     .then ( result => {
-                        result.json()
+                        result.json ()
                             .then ( json => {
-                                this.setState ( { pages_database: json } );
+                                this.setState ({ pages_database: json });
                             });
                     });
         }
@@ -72,34 +72,34 @@ class PokeSAG_Client extends React.Component
     {
         if (this.state.settings_class == "settings hidden")
         {
-            this.setState({settings_class: "settings visible"});
-            this.setState({hamburger_class: "hamburger_button green"});
+            this.setState ({ settings_class: "settings visible" });
+            this.setState ({ hamburger_class: "hamburger_button green" });
         }
         else
         {
-            this.setState({settings_class: "settings hidden"});
-            this.setState({hamburger_class: "hamburger_button"});
+            this.setState ({ settings_class: "settings hidden" });
+            this.setState ({ hamburger_class: "hamburger_button" });
         }
     }
 
     handle_search_toggle = (is_active) =>
     {
-        this.setState({full_text_search: is_active})
+        this.setState ({ full_text_search: is_active })
     }
 
     handle_refresh_toggle = (is_active) =>
     {
         if (is_active)
         {
-            this.setState({
-                auto_refresh_timer: setInterval(() => this.refresh_data(null), 10000),
+            this.setState ({
+                auto_refresh_timer: setInterval (() => this.refresh_data (null), 10000),
                 auto_refresh: true
             });
         } 
         else 
         {
-            clearInterval(this.state.auto_refresh_timer);
-            this.setState({
+            clearInterval (this.state.auto_refresh_timer);
+            this.setState ({
                 auto_refresh_timer: null,
                 auto_refresh: false
             });
@@ -124,11 +124,13 @@ class PokeSAG_Client extends React.Component
         });
 
         /* Generate page */
-        return <div className="app_container">
+        return (
+            <div className="app_container">
 
                 <div className="toolbar">
                     <input className={this.state.hamburger_class} type="button" value="☰" onClick={this.toggle_settings} />
-                    <input className="search_box" type="text" placeholder="Search…" value={this.state.search_string} onChange={this.update_search_string} onKeyPress={this.handle_search} />
+                    <input className="search_box" type="text" placeholder="Search…" value={this.state.search_string}
+                           onChange={this.update_search_string} onKeyPress={this.handle_search} />
                     <input className="refresh_button" type="button" value="↻" onClick={this.refresh_data} />
                 </div>
 
@@ -152,28 +154,29 @@ class PokeSAG_Client extends React.Component
                     </table>
                 </div>
             </div>
+        )
     }
 }
 
 class SettingButton extends React.Component {
-    constructor(props) {
-        super(props);
-        let stored_state = localStorage.getItem(props.value);
+    constructor (props) {
+        super (props);
+        let stored_state = localStorage.getItem (props.value);
         this.state = {
-            is_active: stored_state ? JSON.parse(stored_state) : props.default_state
+            is_active: stored_state ? JSON.parse (stored_state) : props.default_state
         };
-        this.props.action(this.state.is_active);
+        this.props.action (this.state.is_active);
     }
 
     handle_click = () => {
         /* we use a callback here, since setState is asynchronous */
-        this.setState({is_active: !this.state.is_active}, () => {
-            this.props.action(this.state.is_active);   
-            localStorage.setItem(this.props.value, this.state.is_active);
+        this.setState ({ is_active: !this.state.is_active }, () => {
+            this.props.action (this.state.is_active);
+            localStorage.setItem (this.props.value, this.state.is_active);
         });
     }
 
-    render() {
+    render () {
         return (
             <input className={this.state.is_active ? 'setting green' : 'setting red'}
                    type="button" value={this.props.value} onClick={this.handle_click}  />
